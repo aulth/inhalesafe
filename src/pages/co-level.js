29b2c4ed2fd27ccfc01df7b5f629882154f8388c
aulth <mohdusman.you@gmail.com>
2023-10-20 10:42:48 +0530

    co-level page added

--- src/pages/co-level.js
+++ src/pages/co-level.js
@@ -1,15 +1,20 @@
 import Image from 'next/image'
 import { Inter } from 'next/font/google'
 import CoLevel from '@/components/CoLevel'
+import Head from 'next/head'
 
 const inter = Inter({ subsets: ['latin'] })
 
-export default function Home() {
+export default function page() {
   return (
     <>
+    <Head>
+      <title>CO Level | InHaleSafe</title>
+      <meta name="description" content="Real time Carbon Monoxide Level" />
+    </Head>
       <div className="container mx-auto">
         <h2 className="text-center text-2xl font-bold"><span className='text-blue-400'>InHale</span><span className='text-green-400'>Safe</span></h2>
-        {/* <CoLevel /> */}
+        <CoLevel />
       </div>
     </>
   )
